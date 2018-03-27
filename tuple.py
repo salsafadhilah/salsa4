196f4f1ed80d869eac0a684c15191731e06f0fa2
salsafadhilah <37093417+salsafadhilah@users.noreply.github.com>
2018-03-27 09:59:31 +0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = (1,2,3,4)
+>>> b = (5,6,7,8)
+>>> c = a+b;
+>>> d =c;
+>>> print d
+(1, 2, 3, 4, 5, 6, 7, 8)
+>>> print d[2]
+3
+>>> print d[-3:8]
+(6, 7, 8)
+>>> print len(d)
+8
+>>> 
